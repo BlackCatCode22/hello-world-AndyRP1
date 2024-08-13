6dd99235d5be7950a8983f8edbbbf0dc958a88de
AndyRP1 <0960247@my.scccd.edu>
2024-08-13 11:49:22 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\p\Hello World!");
+        System.out.print("\p\How are you today");
+    }
+}
